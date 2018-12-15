33510337c1667d093b5954f664275f6dbe6ccb99
Andrew Castillo <valax321@gmail.com>
2018-12-15 16:28:37 +1100

    Plugin system

---
+++ Sledge.Plugins/PluginPartCatalog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sledge.Plugins
+{
+    public class PluginPartCatalog : ComposablePartCatalog
+    {
+        private readonly string _pluginDir;
+        private readonly Lazy<AggregateCatalog> _innerCatalog;
+
+        public PluginPartCatalog(string directory)
+        {
+            _pluginDir = directory;
+            _innerCatalog = new Lazy<AggregateCatalog>(CreateInnerCatalog);
+        }
+
+        private AggregateCatalog CreateInnerCatalog()
+        {
+            var parts = new List<ComposablePartCatalog>();
+
+
+        }
+
+        public override IEnumerator<ComposablePartDefinition> GetEnumerator()
+        {
+            return _innerCatalog.Value.GetEnumerator();
+        }
+    }
+}
---
+++ Sledge.Plugins/Settings/PluginSettingsContainer.cs
@@ -0,0 +1,33 @@
+﻿using Sledge.Common.Shell.Settings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sledge.Plugins.Settings
+{
+    [Export(typeof(ISettingsContainer))]
+    public class PluginSettingsContainer : ISettingsContainer
+    {
+        public string Name => "Sledge.Plugins.PluginSettingsContainer";
+
+        [Setting("EnabledPlugins")] public Dictionary<string, bool> toggledPlugins;
+
+        public IEnumerable<SettingKey> GetKeys()
+        {
+            yield return new SettingKey("Plugins", "EnabledPlugins", typeof(Dictionary<string, bool>));
+        }
+
+        public void LoadValues(ISettingsStore store)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreValues(ISettingsStore store)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
